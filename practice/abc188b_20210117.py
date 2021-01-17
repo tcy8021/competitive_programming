3c21d80f5a53efef3673f20b2e5f6090c21bced3
tcy8021 <tsuchiya1005a@gmail.com>
2021-01-18 00:14:22 +0900

    add abc188

---
+++ practice/abc188b_20210117.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+sum = 0
+for i in range(n):
+    sum += A[i] * B[i]
+
+if sum == 0:
+    print("Yes")
+else:
+    print("No")
